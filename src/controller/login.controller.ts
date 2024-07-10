657c5a187923c2765c2592d885c124b89463e874
dharanidhanam <dharaniselvam94@gmail.com>
2024-07-10 16:57:55 +0530

    Update .gitignore to ignore node_modules

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -11,7 +11,7 @@ import *  as TokenManager from "../utils/tokenManager"
 var activity = "Login"
 
 /**
- * @author Manibharathi M 
+ * @author Manibharathi 
  * @date 02-05-2024
  * @param {Object} req 
  * @param {Object} res 
